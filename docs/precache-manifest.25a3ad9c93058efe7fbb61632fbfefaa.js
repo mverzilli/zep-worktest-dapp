0c9edda9aac96bd02da1b86421b7f8f57fc2d066
Martin Verzilli <mverzilli@manas.tech>
2020-02-08 13:19:13 +0100

    Package app to be served in gh pages

---
+++ docs/precache-manifest.25a3ad9c93058efe7fbb61632fbfefaa.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d638842474b9e7a6d77f78b6adc304e",
+    "url": "/zep-worktest-dapp/index.html"
+  },
+  {
+    "revision": "c92623f9359a3c4328d6",
+    "url": "/zep-worktest-dapp/static/css/main.be647c17.chunk.css"
+  },
+  {
+    "revision": "9ad2e162327f2295607a",
+    "url": "/zep-worktest-dapp/static/js/2.617ab757.chunk.js"
+  },
+  {
+    "revision": "b078dacb08dc7384280fc7dd190e9d0e",
+    "url": "/zep-worktest-dapp/static/js/2.617ab757.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c92623f9359a3c4328d6",
+    "url": "/zep-worktest-dapp/static/js/main.c0e03e7d.chunk.js"
+  },
+  {
+    "revision": "e1dfd7f50ccbc9d73df7",
+    "url": "/zep-worktest-dapp/static/js/runtime-main.e71e3fd8.js"
+  },
+  {
+    "revision": "0ee01e142249e5fd869d7e8f76db9fdc",
+    "url": "/zep-worktest-dapp/static/media/MagicToken.0ee01e14.sol"
+  },
+  {
+    "revision": "28b0957a0dd1c494a33218a8b154722c",
+    "url": "/zep-worktest-dapp/static/media/Wallet.28b0957a.sol"
+  },
+  {
+    "revision": "fb24292f48cf17c86a30fcaba13f40f6",
+    "url": "/zep-worktest-dapp/static/media/github.fb24292f.svg"
+  },
+  {
+    "revision": "a23208f684c0b55322901b48612b5a1a",
+    "url": "/zep-worktest-dapp/static/media/magictoken.a23208f6.png"
+  },
+  {
+    "revision": "e1a7f3db95ace0c9b9787d02776cb7e6",
+    "url": "/zep-worktest-dapp/static/media/mail.e1a7f3db.svg"
+  },
+  {
+    "revision": "7ce22c1fee10804b8ec860b8ba325267",
+    "url": "/zep-worktest-dapp/static/media/pic_bg.7ce22c1f.png"
+  },
+  {
+    "revision": "fc85a5be08c8cad16ebad20b0981ddc4",
+    "url": "/zep-worktest-dapp/static/media/sold.fc85a5be.png"
+  },
+  {
+    "revision": "bf0095736c595f3d150eeabdb495198d",
+    "url": "/zep-worktest-dapp/static/media/stater-kits-logo.bf009573.png"
+  },
+  {
+    "revision": "75ee96fabc0fde67dcf1a6e62996d8a3",
+    "url": "/zep-worktest-dapp/static/media/twitter.75ee96fa.svg"
+  },
+  {
+    "revision": "b778f9a858d16cc38e7b2e9e862a3410",
+    "url": "/zep-worktest-dapp/static/media/zeppelin_logo.b778f9a8.svg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a5e326a225ee68d3499cb64689518ffc.js"
+  "/zep-worktest-dapp/precache-manifest.25a3ad9c93058efe7fbb61632fbfefaa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/zep-worktest-dapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
